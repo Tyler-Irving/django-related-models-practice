4802cf5eca8240722328cf19a4d8ed0db64b5b7d
Student <student@donation.local>
2019-11-07 15:24:57 -0600

    Class Django practice

--- app/models.py
+++ app/models.py
@@ -1,2 +1,15 @@
 from django.db import models
 
+
+class Album(models.Model):
+    title = models.TextField()
+    artist = models.TextField()
+
+
+class Song(models.Model):
+    title = models.TextField()
+    seconds = models.IntegerField()
+    album = models.ForeignKey(Album, on_delete=models.CASCADE)
+
+    def formatted_duration(self):
+        pass
--- app/views.py
+++ app/views.py
@@ -1,1 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from app.models import Album, Song
+
+
+def album_list(request):
+    albums = Album.objects.all()
+    return render(request, "album_list.html", {"albums": albums})
+
+
+def album_detail(request, id):
+    album = Album.objects.get(id=id)
+    return render(request, "album_detail.html", {"album": album})
+
+def new_song(request, id):
+    if request.method == "GET":
+        return render(request, "album_detail.html")
+    elif request.method == "POST":
+        album = Song(title=request.POST["title"], seconds=int(request.POST["seconds"]), album_id=id)
+        album.save()
+        return redirect(f"album/{id}", {"album": album})
